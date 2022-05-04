40351542c1fe1cd90fd482ef6d3d990553f11f4e
aqilsyamil <78575059+aqilsyamil@users.noreply.github.com>
2022-05-04 20:36:46 +0900

    Upload files via upload - Aqil
    These are .java files that should be stored under a package name "net.forlg.game" which is under src/main/java

---
+++ ForlgGameWebAppApplication.java
@@ -0,0 +1,13 @@
+package net.forlg.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForlgGameWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForlgGameWebAppApplication.class, args);
+	}
+
+}
