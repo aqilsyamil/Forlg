d94ac325a507e26b4cee6f2f2700573281e6caba
aqilsyamil <78575059+aqilsyamil@users.noreply.github.com>
2022-05-04 20:40:38 +0900

    Add files via upload - Aqil
    These are .java files should be stored under a package name "net.forlg.game" under src/java/test

---
+++ ForlgGameWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.forlg.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForlgGameWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
